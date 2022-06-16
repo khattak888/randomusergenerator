3aa43bc07651d74e8979b4d5146891d882bbc65e
khattak888 <bilalktk868@gmail.com>
2022-06-16 04:54:25 -0700

    26

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from '@reduxjs/toolkit'
+
+import { setupListeners } from '@reduxjs/toolkit/query'
+
+import { usersApi } from '../services/users'
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
+  reducer:{
+    [usersApi.reducerPath]:usersApi.reducer,
   },
-});
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(usersApi.middleware),
+})
+setupListeners(store.dispatch)
